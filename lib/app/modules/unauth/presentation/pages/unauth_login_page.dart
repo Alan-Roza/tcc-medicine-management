a8c990c059dc7e98404b370996554959bc23204a
Alan <alancruz@outlook.com>
2024-04-11 17:17:18 -0300

    feat: add bottom sheet clip

--- lib/app/modules/unauth/presentation/pages/unauth_login_page.dart
+++ lib/app/modules/unauth/presentation/pages/unauth_login_page.dart
@@ -1,19 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class UnauthLoginPage extends StatelessWidget {
   const UnauthLoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('UnauthLoginPage')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.go('/medicine-stock-list'),
-          child: const Text('Go to the Medicine Page'),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: const Color(0xFF5AC6FF),
+              child: const Center(
+                child: Text(
+                  'LOGO',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: const Color(0xFF5AC6FF),
+            child: CustomPaint(
+              size: Size(
+                  width,
+                  height/4.7
+                      .toDouble()),
+              painter: MyPainter(),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: const Center(
+                child: Text(
+                  'Clip Path Example',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+  class MyPainter extends CustomPainter {
+    @override
+    void paint(Canvas canvas, Size size) {
+      Paint paint = Paint();
+      Path path = Path();
+  
+
+      paint.color = Colors.white;
+      path = Path();
+      path.lineTo(0, size.height);
+      path.cubicTo(0, size.height, 0, size.height, 0, size.height);
+      path.cubicTo(0, size.height, size.width * 0.11, size.height * 0.3, size.width * 0.115, size.height * 0.3);
+      path.cubicTo(size.width * 0.16, size.height * 0.04, size.width * 0.27, -0.07, size.width * 0.33, size.height * 0.05);
+      path.cubicTo(size.width * 0.37, size.height * 0.05, size.width * 0.89, size.height * 0.66, size.width * 0.89, size.height * 0.66);
+      path.cubicTo(size.width * 0.95, size.height * 0.72, size.width, size.height * 0.85, size.width, size.height);
+      path.cubicTo(size.width, size.height, 0, size.height, 0, size.height);
+      path.cubicTo(0, size.height, 0, size.height, 0, size.height);
+      canvas.drawPath(path, paint);
+    }
+    
+    @override
+    bool shouldRepaint(CustomPainter oldDelegate) {
+      return true;
+    }
+  }
