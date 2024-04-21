4f11ab746480ede3a67a521cf1ca9e85662fb54a
rozaworks <alancruz@outlook.com>
2024-04-21 19:45:17 -0300

    feat: adjusts in signup page

--- lib/app/modules/unauth/presentation/pages/unauth_signup_page.dart
+++ lib/app/modules/unauth/presentation/pages/unauth_signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tcc_medicine_management/app/modules/unauth/presentation/widgets/unauth_layout_widget.dart';
@@ -12,7 +13,7 @@ class UnauthSignupPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF33ABE9),
       body: UnauthLayoutWidget(
-        dinamicHeight: height/2,
+        dinamicHeight: height / 2,
         logo: const SizedBox(
           width: 200,
           child: Text('OLÁ, SEJA BEM-VINDO!',
@@ -50,8 +51,7 @@ class UnauthSignupPage extends StatelessWidget {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                'Cadastrar'
-                                    .toUpperCase(),
+                                'Cadastrar'.toUpperCase(),
                                 style: const TextStyle(
                                     fontSize: 22,
                                     fontFamily:
@@ -79,78 +79,161 @@ class UnauthSignupPage extends StatelessWidget {
                         },
                       ),
                       Expanded(child: Container()),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Usuário',
-                          prefixIcon: const Icon(Icons.person),
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              // Clear the text field
-                            },
-                          ),
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'E-mail',
-                          prefixIcon: const Icon(Icons.person),
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              // Clear the text field
-                            },
-                          ),
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Senha',
-                          prefixIcon: const Icon(Icons.person),
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              // Clear the text field
-                            },
-                          ),
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Confirmar Senha',
-                          prefixIcon: const Icon(Icons.person),
-                          suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              // Clear the text field
-                            },
-                          ),
-                        ),
-                      ),
-                      
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: const Color(
-                              0xFF00A8FF), // Set the text color to white
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                10), // Set the border radius to 10 pixels
-                          ),
-                        ),
-                        onPressed: () {
-                          context.goNamed('Login');
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 0.75,
-                          height:
-                              51, // Set the width to 80% of the screen width
-                          alignment: Alignment.center, // Center the text
-                          child: const Text(
-                            'Entrar',
-                            style: TextStyle(
-                              fontSize: 16, // Set the font size to 16 pixels
-                            ),
+                      SizedBox(
+                        height: height / 2.21,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Usuário',
+                                    prefixIcon: const Icon(Icons.person),
+                                    suffixIcon: IconButton(
+                                      icon: const Icon(Icons.clear),
+                                      onPressed: () {
+                                        // Clear the text field
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    labelText: 'E-mail',
+                                    prefixIcon: const Icon(Icons.person),
+                                    suffixIcon: IconButton(
+                                      icon: const Icon(Icons.clear),
+                                      onPressed: () {
+                                        // Clear the text field
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Senha',
+                                    prefixIcon: const Icon(Icons.person),
+                                    suffixIcon: IconButton(
+                                      icon: const Icon(Icons.clear),
+                                      onPressed: () {
+                                        // Clear the text field
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5.0),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Confirmar Senha',
+                                    prefixIcon: const Icon(Icons.person),
+                                    suffixIcon: IconButton(
+                                      icon: const Icon(Icons.clear),
+                                      onPressed: () {
+                                        // Clear the text field
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 25.0),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: const Color(0xFF00A8FF),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    context.goNamed('Login');
+                                  },
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.75,
+                                    height: 51,
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      'Entrar',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5.0),
+                                child: RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      const TextSpan(
+                                        text:
+                                            "Cadastrando-se com o TCC, você aceita os ",
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                      TextSpan(
+                                        text: "Termos de Uso",
+                                        style: TextStyle(color: Colors.blue),
+                                        recognizer: TapGestureRecognizer()
+                                          ..onTap = () {
+                                            // Handle terms of use tap
+                                          },
+                                      ),
+                                      const TextSpan(
+                                        text: " e a ",
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                      TextSpan(
+                                        text: "Política de Privacidade",
+                                        style: TextStyle(color: Colors.blue),
+                                        recognizer: TapGestureRecognizer()
+                                          ..onTap = () {
+                                            // Handle privacy policy tap
+                                          },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 15.0),
+                                child: RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      const TextSpan(
+                                        text: "Já possui uma conta? ",
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                      TextSpan(
+                                        text: "Entrar",
+                                        style: TextStyle(color: Colors.blue),
+                                        recognizer: TapGestureRecognizer()
+                                          ..onTap = () {
+                                            // Handle terms of use tap
+                                          },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
