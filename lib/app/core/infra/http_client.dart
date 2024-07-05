a1e5e0d12ac54d6e4f0de9045289927382e372a2
Alan Roza <alancruz@outlook.com>
2024-07-04 22:34:28 -0300

    feat: add more methods to http client

--- lib/app/core/infra/http_client.dart
+++ lib/app/core/infra/http_client.dart
@@ -7,19 +7,31 @@ class HttpClient {
 
   HttpClient() : _client = http.Client();
 
-  Future<http.Response> _sendRequest(String method, String endpoint, {dynamic body}) async {
+  Future<http.Response> _sendRequest(String method, String endpoint,
+      {dynamic body}) async {
     final url = Uri.parse('$_basePath$endpoint');
     final headers = {'Content-Type': 'application/json'};
-    var response = http.Response('',404);
+    var response = http.Response('', 404);
 
     switch (method.toUpperCase()) {
       case 'GET':
         response = await _client.get(url, headers: headers);
         break;
       case 'POST':
-        response = await _client.post(url, headers: headers, body: jsonEncode(body));
+        response =
+            await _client.post(url, headers: headers, body: jsonEncode(body));
         break;
-      // Implement other methods as needed
+      case 'PATCH':
+        response =
+            await _client.patch(url, headers: headers, body: jsonEncode(body));
+        break;
+      case 'PUT':
+        response =
+            await _client.put(url, headers: headers, body: jsonEncode(body));
+        break;
+      case 'DELETE':
+        response = await _client.delete(url, headers: headers);
+        break;
     }
 
     return response;
@@ -31,6 +43,18 @@ class HttpClient {
 
   Future<http.Response> post(String endpoint, {dynamic body}) async {
     return await _sendRequest('POST', endpoint, body: body);
+  }
+
+  Future<http.Response> patch(String endpoint, {dynamic body}) async {
+    return await _sendRequest('PATCH', endpoint, body: body);
+  }
+
+  Future<http.Response> put(String endpoint, {dynamic body}) async {
+    return await _sendRequest('PUT', endpoint, body: body);
+  }
+
+  Future<http.Response> delete(String endpoint) async {
+    return await _sendRequest('DELETE', endpoint);
   }
 
   void close() {
