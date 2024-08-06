a63b91d91c5a8abb19ace7d4b82353f68b922296
Alan <alancruz@outlook.com>
2024-08-06 18:14:17 -0300

    wip

--- lib/app/modules/main_home/presentation/main_home_page.dart
+++ lib/app/modules/main_home/presentation/main_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_svg/svg.dart';
